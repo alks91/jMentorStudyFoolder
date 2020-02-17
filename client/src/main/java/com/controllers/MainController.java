98ffac0865447eddcfa34b3293a0073e560dd732
alekseeyn <52881071+alekseeyn@users.noreply.github.com>
2020-02-17 08:00:52 +0400

    Add files via upload

---
+++ client/src/main/java/com/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String getTable() {
+        return "table";
+    }
+}
---
+++ client/target/classes/static/table.js
@@ -0,0 +1,98 @@
+            //GENERATE TABLE
+    fetch('http://localhost:8080/users')
+        .then(response => response.json())
+        .then(response => $.each(response, function (i) {
+            $('<tr>').html(
+                "<td>" + response[i].id + "</td><td>"
+                + response[i].username + "</td><td>"
+                + response[i].userpassword + "</td><td>"
+                + response[i].authority + "</td><td>"
+                + '<button type="button" data-toggle="modal" onclick="getUserByIdModal('+ response[i].id +')" class="btn btn-primary">Update</button>'
+                + ' <button type="button" id="myBtn" onclick="funcDel(' + response[i].id + ')" class="btn btn-primary">Delete</button>' + "</td>")
+                .appendTo('#myTable');
+        }));
+
+            //MODAL
+    function showFunctionModal(user) {
+        $(document).ready(function () {
+            $('#myModal').modal();
+            $("#userIdInput").val(user.id);
+            $("#userNameInput").val(user.username);
+            $("#passwordInput").val(user.userpassword);
+            $("#roleFormSelect").val(user.authority);
+        });
+    }
+
+async function getUserByIdModal(id) {
+   await fetch('http://localhost:8080/users/' + id)
+       .then(user => user.json())
+       .then(user => showFunctionModal(user))
+}
+
+            //UPDATE
+/*
+    function funcUpdate(id) {
+        const myForm = document.getElementById('formElem');
+
+        myForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            const formData = new FormData(this);
+
+            return fetch('http://localhost:8080/users/' + id, { // файл-обработчик
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json', // отправляемые данные
+                },
+                body: JSON.stringify(formData)
+            })
+        });
+    }
+
+*/
+        function funcUpdate() {
+            const forms = document.getElementsByTagName('form');
+            for (let i = 0; i < forms.length; i++) {
+                forms[i].addEventListener('submit', function(e){
+                    e.preventDefault();
+
+                    let formData = new FormData(this);
+                    //formData = Object.fromEntries(formData);
+                    ajaxSend(formData);
+                    this.reset();
+                    window.location.reload();
+                });
+            }
+
+            const ajaxSend = (formData) => {
+                let roleId = 0
+                if(formData.get('role') === 'ROLE_USER')
+                    roleId = 1;
+                else
+                    roleId = 2;
+                formData.delete('role');
+                formData = Object.fromEntries(formData);
+                fetch('http://localhost:8080/users/' + roleId, { // файл-обработчик
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json', // отправляемые данные
+                    },
+                    body: JSON.stringify(formData),
+                })
+                    .catch(error => console.error(error))
+            };
+        }
+
+
+
+
+            //DELETE
+
+function funcDel(id) {
+    fetch('http://localhost:8080/users/' + id, {
+        method: 'delete',
+    }).then(() => location.reload());
+}
+
+
+
